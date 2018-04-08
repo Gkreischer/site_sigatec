ad6d32cba0ebf9c0d9ae0f41535e5b4e9fdc75c5
Gkreischer <gustavokreischer@gmail.com>
2018-04-07 22:57:38 -0300

    iniciando sidebar

--- sistema/js/main.js
+++ sistema/js/main.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
 });
