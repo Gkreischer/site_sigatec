144bf26fffae159750ab6da2608e3e587fbf7074
Gkreischer <gustavokreischer@gmail.com>
2018-04-12 23:23:16 -0300

    Cadastro de Clientes
    Iniciando o modulo de clientes.

--- sistema/js/main.js
+++ sistema/js/main.js
@@ -2,4 +2,44 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
+
+    // Cadastro de clientes
+    $('#form_cd_clientes').submit( function (e){
+        
+        // Aponta para o formulario do cadastro de clientes
+        var formulario_data = $(this);
+        // Monta o objeto cadastro de clientes com o nome de formdata
+        var formdata = new FormData(formulario_data[0]);
+        
+        $.ajax({
+        
+            url: './../php/debug_dados.php',
+            type: 'POST',
+            encytpe: 'multipart/form-data',
+            cache: 'false',
+            data: formdata,
+            contentType: false,
+            processData: false,
+            dataType: 'json',
+
+            success: function(resultado){
+
+                console.log(resultado);
+            },
+            beforeSend: function(){
+
+            },
+            error: function(result){
+                alert('Erro no envio');
+            }
+
+        });
+
+        
+        
+        console.log('Enviado formulario de cadastro de clientes');
+
+        // Desativa reload da página
+        e.preventDefault();
+    });
 });
